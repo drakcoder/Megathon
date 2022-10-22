25bb8de3cd2100fdf688a5d1f167cffc3ada6668
drakcoder <sairamgundala2000@gmail.com>
2022-10-22 22:24:05 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,3 @@
+import translators as ts
+
+print(ts.bing('आज मौसम अच्छा है। इसलिए हम आज खेल सकते हैं!')) # default: from_language='auto', to_language='en'
---
+++ test1.py
@@ -0,0 +1,40 @@
+import speech_recognition as sr
+import translators as ts
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+def takeCommandHindi():
+         
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+          
+
+        print('Listening')
+        r.pause_threshold = 0.6
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)  
+        try:
+            print("Recognizing")
+            Query = r.recognize_google(audio, language='hi-In')
+              
+            print("the query is printed='", Query, "'")
+            print(ts.bing(Query))
+          
+
+        except Exception as e:
+            print(e)  
+            print("Say that again sir")
+            return "None"
+        return Query
+
+# takeCommandHindi()
+
+def getSong(song):
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="1bbc26bf3d92460ba5dec599b34cf3ab",
+                                                           client_secret="6e0aaf498fed442388c0a669ff34c25f"))
+
+    results = sp.search(q=song, limit=20)
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
+
+getSong("stranger things")
